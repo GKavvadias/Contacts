9ced9565b19395f7dab8015ea06b7788521155ff
george kavvas <il_gato_nero@hotmail.com>
2022-07-24 11:58:19 +0300

    Add Avatar.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
-function App(props) {
+function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
